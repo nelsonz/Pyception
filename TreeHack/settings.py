6a36c3c44e78a816c557bed3e9391c056ab361a4
Nelson <nelsonz@acm.org>
2011-10-22 15:36:53 -0700

    migrated to postgresql

--- TreeHack/settings.py
+++ TreeHack/settings.py
@@ -25,11 +25,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'treehack.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres://aeduhqcezu',                      # Or path to database file if using sqlite3.
+        'USER': 'aeduhqcezu',                      # Not used with sqlite3.
+        'PASSWORD': 'xrwJ7ijerGxcgED7uVgY',                  # Not used with sqlite3.
+        'HOST': 'ec2-107-20-155-141.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
